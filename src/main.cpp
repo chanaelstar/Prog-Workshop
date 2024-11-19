093951467d3928d1a113c1641466e1b2961b5221
chanaelstar <chanaelstar@gmail.com>
2024-11-19 17:00:12 +0100

    Mise à jour (mardi)

--- src/main.cpp
+++ src/main.cpp
@@ -59,7 +59,7 @@ void negatif(sil::Image &image){
 
 // Dégradé
 void gradiant(sil::Image &image){
-    ;
+    
     for (int x{0}; x < image.width(); ++x)
     {
         for (int y{0}; y < image.height(); ++y)
@@ -115,9 +115,200 @@ void rotating(sil::Image&image){
 // RGB split
 void rgb_split(sil::Image&image){
 
+    sil::Image image_split{image};
+
+      for(int x{0}; x < image.width(); x++) {
+        for(int y{0}; y < image.height(); y++) {
+
+            if(x < 30) {
+                image_split.pixel(x, y).b = image.pixel(x + 30, y).b;
+            } else if (x > image.width() - 31) {
+                image_split.pixel(x, y).r = image.pixel(x - 30, y).r;
+            } else {
+                image_split.pixel(x, y).r = image.pixel(x - 30, y).r;
+                image_split.pixel(x, y).b = image.pixel(x + 30, y).b;
+            }
+        }
+    }
+    image = image_split;
 }
 
-//
+// Luminosité (éclaircissement & assombrissement)
+void light_luminosity(sil::Image&image){
+
+    for(int x{0}; x < image.width(); x++) {
+        for(int y{0}; y < image.height(); y++) {
+            image.pixel(x, y).r = std::sqrt(image.pixel(x, y).r);
+            image.pixel(x, y).g = std::sqrt(image.pixel(x, y).g);
+            image.pixel(x, y).b = std::sqrt(image.pixel(x, y).b);
+        }
+    }
+}
+
+void dark_Luminosity(sil::Image &image) {
+    for(int x{0}; x < image.width(); x++) {
+        for(int y{0}; y < image.height(); y++) {
+            image.pixel(x, y).r = std::pow(image.pixel(x, y).r, 2);
+            image.pixel(x, y).g = std::pow(image.pixel(x, y).g, 2);
+            image.pixel(x, y).b = std::pow(image.pixel(x, y).b, 2);
+        }
+    }
+}
+
+// Disque 
+void disk(sil::Image&image){
+
+    int a{image.width()/2};
+    int b{image.height()/2};
+    int r{100};
+
+    for (int x{0}; x < 500; ++x)
+    {
+        for (int y{0}; y < 500; ++y)
+        {
+            if((pow(x-a, 2) + pow(y-b, 2)) < pow(r, 2)) {
+                image.pixel(x, y).r = 1;
+                image.pixel(x, y).g = 0;
+                image.pixel(x, y).b = 0;
+            }
+            else{
+                image.pixel(x, y).r = 1;
+                image.pixel(x, y).g = 1;
+                image.pixel(x, y).b = 1;
+                // drapeau japon 
+            }
+            
+        }
+    }
+}
+
+// Cercle 
+void circle(sil::Image&image){
+
+    int a{image.width()/2};
+    int b{image.height()/2};
+    int r{100};
+    int thickness{10};
+
+    for (int x{0}; x < image.width(); x++)
+
+    {
+        for (int y{0}; y < image.height(); y++)
+
+        {
+            if(pow(x-a, 2) + pow(y-b, 2) < pow(r - thickness, 2)) {
+                image.pixel(x, y).r = 0;
+                image.pixel(x, y).g = 0;
+                image.pixel(x, y).b = 0;
+            }
+            else if (pow(x-a, 2) + pow(y-b, 2) < pow(r, 2)){
+                image.pixel(x, y).r = 1;
+                image.pixel(x, y).g = 1;
+                image.pixel(x, y).b = 1;
+            }
+
+            else{
+                image.pixel(x, y).r = 0;
+                image.pixel(x, y).g = 0;
+                image.pixel(x, y).b = 0;
+
+            }
+
+        }
+    }
+}
+
+// Animation 
+void animation(sil::Image&image){
+
+    int a{0};
+    int b{image.height()/2};
+    int r{100};
+
+    while (a < 500)
+    {
+        a++;
+        for (int x{0}; x < 500; ++x)
+        {
+            for (int y{0}; y < 500; ++y)
+            {
+                if((pow(x-a, 2) + pow(y-b, 2)) < pow(r, 2)) {
+                    image.pixel(x, y).r = 1;
+                    image.pixel(x, y).g = 0;
+                    image.pixel(x, y).b = 0;
+                }
+                else{
+                    image.pixel(x, y).r = 1;
+                    image.pixel(x, y).g = 1;
+                    image.pixel(x, y).b = 1; 
+                }
+                
+            }
+        }
+        image.save("output/anim/animation"+std::to_string(a)+".png");
+    }
+    
+   
+        
+    }
+    
+    
+
+
+// Rosace
+void rosace(sil::Image &image)
+{
+
+    int r{100};
+    int thickness{5};
+
+    for (int x{0}; x < image.width(); x++)
+
+    {
+        for (int y{0}; y < image.height(); y++)
+
+        {
+
+            int dx = x - image.width() / 2;
+            int dy = y - image.height() / 2;
+            if ((dx * dx + dy * dy <= r * r) && (dx * dx + dy * dy > (r - thickness) * (r - thickness)))
+            {
+                image.pixel(x, y).r = 1.0f;
+                image.pixel(x, y).g = 1.0f;
+                image.pixel(x, y).b = 1.0f;
+            }
+        }
+    }
+    int centreX = image.width() / 2;
+    int centreY = image.height() / 2;
+    float angleStep = 2 * 3.14 / 6;
+    for (int i = 0; i < 6; i++)
+    {
+        float angle = i * angleStep;
+        int offsetX = static_cast<int>(r * cos(angle));
+        int offsetY = static_cast<int>(r * sin(angle));
+
+        for (int x = 0; x < image.width(); x++)
+        {
+            for (int y = 0; y < image.height(); y++)
+            {
+                int dx = x - centreX - offsetX;
+                int dy = y - centreY - offsetY;
+
+                if ((dx * dx + dy * dy <= r * r) && (dx * dx + dy * dy > (r - thickness) * (r - thickness)))
+                {
+                    image.pixel(x, y).r = 1.0f;
+                    image.pixel(x, y).g = 1.0f;
+                    image.pixel(x, y).b = 1.0f;
+                }
+            }
+        }
+    }
+}
+
+// 
+
+
 int main()
 {
     {
@@ -163,7 +354,35 @@ int main()
     {
         sil::Image image{"images/logo.png"};
         rgb_split(image); 
-        image.save("output/pouet.png"); // Sauvegarde l'image
+        image.save("output/rgb_split.png"); // Sauvegarde l'image
     }
-    // bhjgj
+    {
+        sil::Image image{"images/photo.jpg"};
+        light_luminosity(image); 
+        image.save("output/light_luminosity.jpg"); // Sauvegarde l'image
+    }
+    {
+        sil::Image image{"images/photo.jpg"};
+        dark_Luminosity(image); 
+        image.save("output/dark_luminosity.jpg"); // Sauvegarde l'image
+    }
+    {
+        sil::Image image{500, 500}; // Lis l'image
+        disk(image); // Utilise la fonction pour modifier l'image
+        image.save("output/disk.png"); // Sauvegarde l'image
+    }
+    {
+        sil::Image image{500, 500}; // Lis l'image
+        circle(image); // Utilise la fonction pour modifier l'image
+        image.save("output/circle.png"); // Sauvegarde l'image
+    }
+    {
+        sil::Image image{500, 500}; // Lis l'image
+        animation(image); // Utilise la fonction pour modifier l'image
+    }
+    {
+        sil::Image image{500, 500}; // Lis l'image
+        rosace(image); // Utilise la fonction pour modifier l'image
+        image.save("output/rosace.png"); // Sauvegarde l'image
+    }
 }
